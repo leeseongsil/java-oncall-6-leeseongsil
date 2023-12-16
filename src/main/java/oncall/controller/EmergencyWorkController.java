5d26cfa465c2a4e3908baa4f1ae7f6756f17b1fa
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-12-16 16:37:29 +0900

    feat (EmergencyWorkController) : 전반적인 제어 기능 구현
    - 입력 부분에서 예외가 발생할 경우 해당 부분부터 다시 입력받는다

---
+++ src/main/java/oncall/controller/EmergencyWorkController.java
@@ -0,0 +1,42 @@
+package oncall.controller;
+
+import java.util.List;
+import java.util.function.Supplier;
+import oncall.domain.Day;
+import oncall.domain.EmergencyWork;
+import oncall.dto.WorkOrder;
+import oncall.dto.WorkSetting;
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
+public class EmergencyWorkController {
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void run() {
+        List<Day> days = readRepeatedlyUntilNoException(this::readDays);
+        EmergencyWork emergencyWork = readRepeatedlyUntilNoException(() -> readEmergencyWork(days));
+
+        outputView.printEmergencyWork(emergencyWork.generate());
+    }
+
+    private List<Day> readDays() {
+        WorkSetting setting = inputView.inputMonthAndStartDate();
+        return Day.createList(setting.month(), setting.startDayOfWeek());
+    }
+
+    private EmergencyWork readEmergencyWork(List<Day> days) {
+        WorkOrder order = inputView.inputWorkOrder();
+        return new EmergencyWork(order.weekdayOrder(), order.weekendOrder(), days);
+    }
+
+    private <T> T readRepeatedlyUntilNoException(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException exception) {
+            outputView.printErrorMessage(exception);
+            return readRepeatedlyUntilNoException(supplier);
+        }
+    }
+}
