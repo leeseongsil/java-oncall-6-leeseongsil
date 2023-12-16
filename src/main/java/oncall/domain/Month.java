34c91ac12e2b0bc4d17a28a56f464ce63c73f07c
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-12-16 14:15:10 +0900

    feat (Month) : 각 달마다 최대 날짜, 법정 공휴일로 구성함
    - 2월은 28일이 최대 날짜로 고정
    - 법정 공휴일은 주어진 날을 사용

---
+++ src/main/java/oncall/domain/Month.java
@@ -0,0 +1,28 @@
+package oncall.domain;
+
+import java.util.List;
+
+public enum Month {
+    JANUARY(1, 31, List.of(1)),
+    FEBRUARY(2, 28, List.of()),
+    MARCH(3, 31, List.of(1)),
+    APRIL(4, 30, List.of()),
+    MAY(5, 31, List.of(5)),
+    JUNE(6, 30, List.of(6)),
+    JULY(7, 31, List.of()),
+    AUGUST(8, 31, List.of(15)),
+    SEPTEMBER(9, 30, List.of()),
+    OCTOBER(10, 31, List.of(3, 9)),
+    NOVEMBER(11, 30, List.of()),
+    DECEMBER(12, 31, List.of(25));
+
+    private final int order;
+    private final int maxDate;
+    private final List<Integer> holidays;
+
+    Month(int order, int maxDate, List<Integer> holidays) {
+        this.order = order;
+        this.maxDate = maxDate;
+        this.holidays = holidays;
+    }
+}
