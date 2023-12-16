a4226312a99f1460a1e135a352557d8d51a8d2d3
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-12-16 14:59:59 +0900

    feat (Day) : Month와 시작 요일을 통해 해당 달 날짜 리스트 만드는 기능 구현

--- src/main/java/oncall/domain/Day.java
+++ src/main/java/oncall/domain/Day.java
@@ -1,15 +1,26 @@
 package oncall.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Day {
 
     private final Month month;
     private final int date;
     private final DayOfWeek dayOfWeek;
 
-    public Day(Month month, int date, DayOfWeek dayOfWeek) {
+    private Day(Month month, int date, DayOfWeek dayOfWeek) {
         this.month = month;
         this.date = date;
         this.dayOfWeek = dayOfWeek;
+    }
+
+    public static List<Day> createList(Month month, DayOfWeek startDayOfWeek) {
+        return Stream.iterate(new Day(month, 1, startDayOfWeek),
+                        day -> new Day(month, day.date + 1, day.dayOfWeek.findNextDayOfWeek()))
+                .limit(month.getMaxDate())
+                .collect(Collectors.toList());
     }
 
     public boolean isHoliday() {
