8a91af6873a8a9586a8a80acd1947d2eed505357
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-12-16 17:40:30 +0900

    test (TODO) : 단위 테스트 할 메서드 일부 구현
    - 해당 메서드를 바탕으로 단위 테스트 구현 예정

---
+++ src/test/java/oncall/domain/DayTest.java
@@ -0,0 +1,21 @@
+package oncall.domain;
+
+import org.junit.jupiter.api.Test;
+
+class DayTest {
+
+    @Test
+    void isHolidayTest() {
+        // TODO 단위 테스트 구현
+    }
+
+    @Test
+    void isShowHoliday() {
+        // TODO 단위 테스트 구현
+    }
+
+    @Test
+    void createListTest() {
+        // TODO 단위 테스트 구현
+    }
+}
---
+++ src/test/java/oncall/domain/EmergencyWorkTest.java
@@ -0,0 +1,17 @@
+package oncall.domain;
+
+import org.junit.jupiter.api.Test;
+
+class EmergencyWorkTest {
+
+    @Test
+    void validationTest() {
+        // TODO 단위 테스트 구현
+        // 이름 중복 테스트, 명수 테스트, 이름 테스트, 주말-주중 구성 테스트
+    }
+
+    @Test
+    void generateTest() {
+        // TODO 단위 테스트 구현
+    }
+}
